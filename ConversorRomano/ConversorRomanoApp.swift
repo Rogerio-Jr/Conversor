63c2eb3b436431bf2b633ff5b0095cd49d30f842
Rogerio-Jr <rogeriojunior0101@gmail.com>
2023-11-19 23:26:34 -0400

    Initial Commit

---
+++ ConversorRomano/ConversorRomanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversorRomanoApp.swift
+//  ConversorRomano
+//
+//  Created by Rogério Júnior on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ConversorRomanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
